7f0325cf9954f43d76629c3d4ce9c5dc9a4aa87a
feraistanti <fistanti551@gmail.com>
2024-05-10 16:58:32 +0700

    Prosedur untuk peek

--- stackusingLinkedList_0083/stackusingLinkedList_0083.cpp
+++ stackusingLinkedList_0083/stackusingLinkedList_0083.cpp
@@ -32,3 +32,10 @@ public:
 	void pop() {
 		if (isEmpty()) {
 			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+	}
+
+
+	void peek() {
